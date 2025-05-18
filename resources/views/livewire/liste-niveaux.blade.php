9ab79fddd0f9c2df10a0f9a66c1ae99530948a02
Elm177 <ouss123maelmouh@gmail.com>
2025-05-18 18:58:11 +0100

    Generate the Niveaux list component

---
+++ resources/views/livewire/liste-niveaux.blade.php
@@ -0,0 +1,3 @@
+<div>
+   Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet nulla eius quam quod ipsam adipisci eum, veritatis culpa amet et a exercitationem aperiam iusto? Quia dolore doloremque doloribus veniam optio.
+</div>
