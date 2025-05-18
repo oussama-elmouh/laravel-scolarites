2f0128c97530773a694fb333bf6d0a579b87914c
Elm177 <ouss123maelmouh@gmail.com>
2025-05-18 21:13:26 +0100

    create model show list withe Tailwind

--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -1,3 +1,63 @@
-<div>
-   Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere dolore saepe, eligendi rerum odit corrupti molestiae laudantium perspiciatis deserunt animi incidunt sunt inventore quas, debitis suscipit laborum quod iure. Adipisci!
+<div class="mt-5">
+    <div class="bg-white overflow-hidden shadow-xll sm:rounded-lg p-4">
+    {{-- Ritre et Button crée --}}
+    <div class="flex justify-between itmes-center">
+        <h4>Listes des Année scolaires</h4>
+        <a href="#" class="bg-blue-500 rounded-md p-2 text-sm text-white">
+            Nouvelle Année Scolaire
+        </a>
+    </div>
+
+    <div class="flex flex-col">
+        {{-- Message qui apparaitra aprés operation  --}}
+        <div class="block p-2 bg-green-500 text-white rounded-sm shadow-sm mt-2">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo molestias iusto numquam iure nesciunt voluptas, magni nostrum, harum praesentium sequi beatae provident eligendi quae magnam? Earum dignissimos ratione cumque sequi?
+        </div>
+    </div>   
+
+    {{-- Styliser le tableau --}}
+    <div class="overflow-x-auto ">
+        <div class="py-4 inline-block min-w-full">
+            <div class="overflow-hidden">
+                <table class="min-w-full text-center">
+
+                    <thead class="border-b bg-gray-50">
+                        <tr class ="border-b-2 border-gray-100">
+                            <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle</th>
+                            <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle</th>
+                            <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle</th>
+                            <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle</th>
+                        </tr>
+                    </thead>  
+                    <tbody>
+                        <tr class="border-b-2 border-gray-100">
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>   
+                        </tr>    
+                        <tr class="border-b-2 border-gray-100">
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>   
+                        </tr>    
+                        <tr class="border-b-2 border-gray-100">
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>   
+                        </tr>    
+                        <tr class="border-b-2 border-gray-100">
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>
+                           <td class="text-sm font-medium text-gray-900 px-6 py-6">Exemple Libelle</td>   
+                        </tr>    
+                    </tbody>  
+                </table>
+            </div>
+        </div>
+    </div>
+    </div>
 </div>
